a2698cb246d42cd71e232c52b37da35b8b952d54
kaiken112 <133145067+kaiken112@users.noreply.github.com>
2026-01-28 06:14:53 +0000

    Update socket URL for production

--- src/sockets.js
+++ src/sockets.js
@@ -3,17 +3,17 @@ import io from "socket.io-client";
 // export const socket = io.connect("http://localhost:5000");
 //export const socket = io("https://juvenile-closed-stork.glitch.me", {
 
-export const socket = io("https://shadowverse-server.onrender.com/", {
-  transports: ["websocket"],
+export const socket = io("https://cleint-nhck.onrender.com", {
+    transports: ["websocket"],
 });
 
 socket.on("connect_error", (err) => {
-  // the reason of the error, for example "xhr poll error"
-  console.log(err.message);
+    // the reason of the error, for example "xhr poll error"
+    console.log(err.message);
 
-  // some additional description, for example the status code of the initial HTTP response
-  console.log(err.description);
+    // some additional description, for example the status code of the initial HTTP response
+    console.log(err.description);
 
-  // some additional context, for example the XMLHttpRequest object
-  console.log(err.context);
+    // some additional context, for example the XMLHttpRequest object
+    console.log(err.context);
 });
